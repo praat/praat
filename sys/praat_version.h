00d6b7736dc962f88f88cb497d9b602e3109eb59
Paul Boersma <paul.boersma@uva.nl>
2016-02-11 00:44:52 +0100

    for 6.0.14

--- sys/praat_version.h
+++ sys/praat_version.h
@@ -1,5 +1,5 @@
-#define PRAAT_VERSION_STR 6.0.13
-#define PRAAT_VERSION_NUM 6013
+#define PRAAT_VERSION_STR 6.0.14
+#define PRAAT_VERSION_NUM 6014
 #define PRAAT_YEAR 2016
-#define PRAAT_MONTH January
-#define PRAAT_DAY 31
+#define PRAAT_MONTH February
+#define PRAAT_DAY 11
