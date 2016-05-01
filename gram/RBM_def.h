d739cd25ccfbcc4a2f5f44346457445156fc1633
Paul Boersma <paul.boersma@uva.nl>
2016-05-01 13:27:37 +0200

    RBM

--- gram/RBM_def.h
+++ gram/RBM_def.h
@@ -17,95 +17,29 @@
  */
 
 
-#define ooSTRUCT NetworkNode
-oo_DEFINE_STRUCT (NetworkNode)
+#define ooSTRUCT RBM
+oo_DEFINE_CLASS (RBM, Daata)
 
-	oo_DOUBLE (x)
-	oo_DOUBLE (y)
-	oo_BOOLEAN (clamped)
-	oo_DOUBLE (activity)
+	oo_BOOL (inputsAreBinary)
+	oo_LONG (numberOfInputNodes)
+	oo_LONG (numberOfOutputNodes)
+	oo_DOUBLE_MATRIX (weights, numberOfInputNodes, numberOfOutputNodes)
+	oo_DOUBLE_VECTOR (inputBiases, numberOfInputNodes)
+	oo_DOUBLE_VECTOR (outputBiases, numberOfOutputNodes)
 
-	#if ! oo_READING && ! oo_WRITING
-		oo_DOUBLE (excitation)
+	#if oo_DECLARING
+		oo_DOUBLE_VECTOR (inputActivities, numberOfInputNodes)
+		oo_DOUBLE_VECTOR (outputActivities, numberOfOutputNodes)
+		oo_DOUBLE_VECTOR (inputReconstruction, numberOfInputNodes)
+		oo_DOUBLE_VECTOR (outputReconstruction, numberOfOutputNodes)
 	#endif
-
-oo_END_STRUCT (NetworkNode)
-#undef ooSTRUCT
-
-
-#define ooSTRUCT NetworkConnection
-oo_DEFINE_STRUCT (NetworkConnection)
-
-	oo_LONG (nodeFrom)
-	oo_LONG (nodeTo)
-	oo_DOUBLE (weight)
-	oo_FROM (1)
-		oo_DOUBLE (plasticity)
-	oo_ENDFROM
-
-	#if oo_READING
-		if (formatVersion < 1) {
-			plasticity = 1.0;
-		}
-	#endif
-
-oo_END_STRUCT (NetworkConnection)
-#undef ooSTRUCT
-
-
-#define ooSTRUCT Network
-oo_DEFINE_CLASS (Network, Daata)
-
-	oo_DOUBLE (minimumActivity)
-	oo_DOUBLE (maximumActivity)
-	oo_FROM (3)
-		oo_BYTE (dummyActivitySpreadingRule)
-	oo_ENDFROM
-	oo_FROM (4)
-		oo_DOUBLE (shunting)
-		oo_ENUM (kNetwork_activityClippingRule, activityClippingRule)
-	oo_ENDFROM
-	oo_DOUBLE (spreadingRate)
-	oo_DOUBLE (activityLeak)
-	oo_DOUBLE (minimumWeight)
-	oo_DOUBLE (maximumWeight)
-	oo_FROM (2)
-		oo_BYTE (dummyWeightUpdateRule)
-	oo_ENDFROM
-	oo_DOUBLE (learningRate)
-	oo_FROM (5)
-		oo_DOUBLE (instar)
-		oo_DOUBLE (outstar)
-	oo_ENDFROM
-	oo_DOUBLE (weightLeak)
-
-	#if oo_READING
-		if (formatVersion < 6) {
-			if (formatVersion < 5) {
-				if (our learningRate != 0.0) our weightLeak /= our learningRate;
-				if (our dummyWeightUpdateRule == 1) our instar = 1.0, our outstar = 0.0;
-				if (our dummyWeightUpdateRule == 2) our instar = 0.0, our outstar = 1.0;
-				if (our dummyWeightUpdateRule == 3) our instar = 0.5, our outstar = 0.5;
-			}
-			our activityLeak = - our activityLeak;   // convert self-excitation to activity leak
-		}
-	#endif
-
-	oo_DOUBLE (xmin)
-	oo_DOUBLE (xmax)
-	oo_DOUBLE (ymin)
-	oo_DOUBLE (ymax)
-	oo_LONG (numberOfNodes)
-	oo_STRUCT_VECTOR (NetworkNode, nodes, numberOfNodes)
-	oo_LONG (numberOfConnections)
-	oo_STRUCT_VECTOR (NetworkConnection, connections, numberOfConnections)
 
 	#if oo_DECLARING
 		void v_info ()
 			override;
 	#endif
 
-oo_END_CLASS (Network)
+oo_END_CLASS (RBM)
 #undef ooSTRUCT
 
 
