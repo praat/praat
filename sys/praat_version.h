dfeb10e932832c1b7cdea932f2b1a7102acceb31
Paul Boersma <paul.boersma@uva.nl>
2016-09-25 18:46:12 +0200

    for version 6.0.21

--- sys/praat_version.h
+++ sys/praat_version.h
@@ -1,5 +1,5 @@
-#define PRAAT_VERSION_STR 6.0.20
-#define PRAAT_VERSION_NUM 6020
+#define PRAAT_VERSION_STR 6.0.21
+#define PRAAT_VERSION_NUM 6021
 #define PRAAT_YEAR 2016
 #define PRAAT_MONTH September
-#define PRAAT_DAY 3
+#define PRAAT_DAY 25
