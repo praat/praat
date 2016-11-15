dbffb4d9d75523ddbb7e215fa636679a63aeb201
David Weenink <David.Weenink@uva.nl>
2016-11-15 11:55:48 +0100

    make c99 compiler happy

--- external/gsl/gsl__config.h
+++ external/gsl/gsl__config.h
@@ -24,7 +24,7 @@
 /* Define to 1 if you have the declaration of `feenableexcept', and to 0 if
    you don't. */
 #if defined(linux)
-	#define HAVE_DECL_FEENABLEEXCEPT 1
+	#define HAVE_DECL_FEENABLEEXCEPT 0
 #else
 	#define HAVE_DECL_FEENABLEEXCEPT 0
 #endif
