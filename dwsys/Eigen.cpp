19757029e4ad0972292a214f4f33bade0a65ac62
David Weenink <David.Weenink@uva.nl>
2017-05-13 15:33:53 +0200

    correct typo

--- dwsys/Eigen.cpp
+++ dwsys/Eigen.cpp
@@ -1,6 +1,6 @@
 /* Eigen.cpp
  *
- * Copyright (C) 1993-2016 David Weenink
+ * Copyright (C) 1993-2017 David Weenink
  *
  * This code is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -395,7 +395,7 @@ void Eigen_drawEigenvalues (Eigen me, Graphics g, long first, long last, double 
 			double tmp = ymin; ymin = ymax; ymax = tmp;
 		}
 		if (ymin == ymax) { // only one eigenvalue
-			ymin -= 0.1 * ymin; ymax = ymax += 0.1 * ymax;
+			ymin -= 0.1 * ymin; ymax += 0.1 * ymax;
 		}
 	}
 	Graphics_setInner (g);
