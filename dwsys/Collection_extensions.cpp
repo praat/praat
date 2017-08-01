f9cd73aaca3d448614a0d83e551ed8962b7f57c3
David Weenink <David.Weenink@uva.nl>
2017-08-01 20:35:21 +0200

    NUM.h is removed

--- dwsys/Collection_extensions.cpp
+++ dwsys/Collection_extensions.cpp
@@ -1,6 +1,6 @@
 /* Collection_extensions.cpp
  *
- * Copyright (C) 1994-2011, 2015-2016 David Weenink
+ * Copyright (C) 1994-2011, 2015-2017 David Weenink
  *
  * This code is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
