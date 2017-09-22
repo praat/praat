bd0eb88e47f486128d4107134c346a86981c207f
David Weenink <David.Weenink@uva.nl>
2017-09-22 15:00:26 +0200

    Get maximum distance

--- dwtools/Distance.h
+++ dwtools/Distance.h
@@ -2,7 +2,7 @@
 #define _Distance_h_
 /* Distance.h
  *
- * Copyright (C) 1993-2011, 2015 David Weenink
+ * Copyright (C) 1993-2011, 2015, 2017 David Weenink
  *
  * This code is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -27,5 +27,7 @@ Thing_define (Distance, Proximity) {
 autoDistance Distance_create (long numberOfPoints);
 
 void Distance_drawDendogram (Distance me, Graphics g, int method);
+
+double Distance_getMaximumDistance (Distance me);
 
 #endif /* _Distance_h_ */
