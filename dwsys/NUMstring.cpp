bd95abe56398441ed43388a6a86ef4a959ba39c3
David Weenink <David.Weenink@uva.nl>
2017-09-26 21:59:14 +0200

    return nullptr if numberOfElements == 0

--- dwsys/NUMstring.cpp
+++ dwsys/NUMstring.cpp
@@ -450,6 +450,9 @@ static long *getElementsOfRanges (const char32 *ranges, long maximumElement, lon
 	/*
 	 * Create room for the elements.
 	 */
+	if (*numberOfElements == 0) {
+		return nullptr;
+	}
 	autoNUMvector <long> elements (1, *numberOfElements);
 	/*
 	 * Store the elements.
@@ -507,7 +510,7 @@ static void NUMlvector_getUniqueNumbers (long *numbers, long *p_numberOfElements
 
 long *NUMstring_getElementsOfRanges (const char32 *ranges, long maximumElement, long *numberOfElements, long *numberOfMultiples, const char32 *elementType, bool sortedUniques) {
 	autoNUMvector<long> elements (getElementsOfRanges (ranges, maximumElement, numberOfElements, elementType), 1);
-	if (sortedUniques) {
+	if (sortedUniques && *numberOfElements > 0) {
 		NUMlvector_getUniqueNumbers (elements.peek(), numberOfElements, numberOfMultiples);
 	}
 	return elements.transfer();
