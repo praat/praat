16912594eeef2c523c1f1851b9274eda0ead7cf2
Paul Boersma <paul.boersma@uva.nl>
2017-10-14 18:46:32 +0200

    update comments on undefined

--- sys/melder.h
+++ sys/melder.h
@@ -658,35 +658,17 @@ void NUMscale (double *x, double xminfrom, double xmaxfrom, double xminto, doubl
 // Instead we use the 40 digits computed by Johann von Soldner in 1809.
 #define NUM_euler  0.5772156649015328606065120900824024310422
 
-/*
-	Ideally, `undefined` should be #defined as NAN (or 0.0/0.0),
-	because that would make sure that
-		1.0 / undefined
-	evaluates as undefined.
-	However, we cannot do that as long as Praat contains any instances of
-		if (x == undefined) { ... }
-	because that condition would evaluate as false even if x were undefined
-	(because NAN is unequal to NAN).
-	Therefore, we must define, for the moment, `undefined` as positive infinity,
-	because positive infinity can be compared to itself
-	(i.e. Inf is equal to Inf). The drawback is that
-		1.0 / undefined
-	will evaluate as 0.0, i.e. this version of `undefined` does not propagate properly.
-*/
-#define undefined  (0.0/0.0)
-//#define undefined  NAN   /* a future definition? */
-//#define undefined  (0.0/0.0)   /* an alternative future definition */
+#define undefined  (0.0/0.0)   /* NaN */
 
 /*
-	Ideally, isdefined() should capture not only `undefined`, but all infinities and nans.
+	isdefined() shall capture not only `undefined`, but all infinities and NaNs.
 	This can be done with a single test for the set bits in 0x7FF0000000000000,
 	at least for 64-bit IEEE implementations. The correctness of this assumption is checked in sys/praat.cpp.
-	The portable version of isdefined() would involve both isinf() and isnan(),
+	The portable version of isdefined() involves both isinf() and isnan(), or perhaps just isfinite(),
 	but that would be slower (as tested in fon/Praat_tests.cpp)
 	and it would also get into problems on some platforms whenever both <cmath> and <math.h> are included,
 	as in dwsys/NUMcomplex.cpp.
 */
-//#define isdefined(x)  ((x) != NUMundefined)   /* an old definition, not good at capturing nans */
 //inline static bool isdefined (double x) { return ! isinf (x) && ! isnan (x); }   /* portable */
 inline static bool isdefined (double x) { return ((* (uint64_t *) & x) & 0x7FF0000000000000) != 0x7FF0000000000000; }
 inline static bool isundef (double x) { return ((* (uint64_t *) & x) & 0x7FF0000000000000) == 0x7FF0000000000000; }
