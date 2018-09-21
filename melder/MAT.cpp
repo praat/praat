7d1847f170d0825636f1aa0bc1aba05b8bd6f076
Paul Boersma <paul.boersma@uva.nl>
2018-09-21 15:33:28 +0200

    vDSP on macOS

--- melder/MAT.cpp
+++ melder/MAT.cpp
@@ -16,11 +16,25 @@
  * along with this work. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#define USE_CBLAS_GEMM  0
+#define USE_GSL_GEMM  0
+#ifdef macintosh
+	#define USE_APPLE_GEMM  1
+#else
+	#define USE_APPLE_GEMM  0
+#endif
+
 #include "melder.h"
 #include "../dwsys/NUM2.h"
-#include "../dwsys/NUMcblas.h"
-//#include "../external/gsl/gsl_matrix_double.h"
-#include "../external/gsl/gsl_blas.h"
+#if USE_CBLAS_GEMM
+	#include "NUMcblas.h"
+#endif
+#if USE_GSL_GEMM
+	#include "../external/gsl/gsl_blas.h"
+#endif
+#if USE_APPLE_GEMM
+	#include <Accelerate/Accelerate.h>
+#endif
 
 void MATcentreEachColumn_inplace (MAT x, double centres []) {
 	autoVEC columnBuffer = VECraw (x.nrow);
@@ -70,40 +84,65 @@ void MATmul_fast_preallocated_ (const MAT& target, const constMAT& x, const cons
 		so it will be moved out of the loop by the compiler.
 	*/
 	for (integer irow = 1; irow <= target.nrow; irow ++) {
-		for (integer icol = 1; icol <= target.ncol; icol ++)
-			target [irow] [icol] = 0.0;
-		for (integer i = 1; i <= x.ncol; i ++)
+		for (integer icol = 1; icol <= target.ncol; icol ++) {
+			PAIRWISE_SUM (longdouble, sum, integer, x.ncol,
+				const double *px = & x [irow] [1];
+				const double *py = & y [1] [icol],
+				(longdouble) *px * (longdouble) *py,
+				(px += 1, py += y.ncol)
+			)
+			target [irow] [icol] = double (sum);
+		}
+	}
+}
+
+void MATmul_fast_preallocated_ (const MAT& target, const constMAT& x, const constMAT& y) {
+	#if USE_CBLAS_GEMM
+		/*
+			This version is 49,0.75,0.32,0.33 ns per multiply-add for size = 1,10,100,1000.
+		*/
+		double alpha = 1.0, beta = 0.0;
+		NUMblas_dgemm ("N", "N", & target.nrow, & target.ncol, & x.ncol, & alpha,
+				(double *) & x [1] [1], & x.nrow, (double *) & y [1] [1], & y.nrow, & beta, & target [1] [1], & target.nrow);
+	#elif USE_GSL_GEMM
+		/*
+			This version is 34,0.72,0.31,0.41(NoTrans;1.34Trans) ns per multiply-add for size = 1,10,100,1000.
+		*/
+		gsl_matrix gslx { (size_t) x.nrow, (size_t) x.ncol, (size_t) x.nrow, (double *) & x [1] [1], nullptr, false };
+		gsl_matrix gsly { (size_t) y.nrow, (size_t) y.ncol, (size_t) y.nrow, (double *) & y [1] [1], nullptr, false };
+		gsl_matrix gsltarget { (size_t) target.nrow, (size_t) target.ncol, (size_t) target.nrow, (double *) & target [1] [1], nullptr, false };
+		gsl_blas_dgemm (CblasTrans, CblasTrans, 1.0, & gslx, & gsly, 0.0, & gsltarget);
+	#elif USE_APPLE_GEMM
+		cblas_dgemm (CblasRowMajor, CblasNoTrans, CblasNoTrans, target.nrow, target.ncol, x.ncol,
+				1.0, & x [1] [1], x.nrow, & y [1] [1], y.nrow, 0.0, & target [1] [1], target.nrow);   // 24,0.71,0.31,0.45
+	#elif 1
+		/*
+			This version is 10,0.76,0.32,0.34 ns per multiply-add for size = 1,10,100,1000.
+
+			The trick is to have the inner loop run along two final indices.
+			Note that the multiplication factor within the inner loop is constant,
+			so it will be moved out of the loop by the compiler.
+		*/
+		for (integer irow = 1; irow <= target.nrow; irow ++) {
 			for (integer icol = 1; icol <= target.ncol; icol ++)
-				target [irow] [icol] += x [irow] [i] * y [i] [icol];
-	}
-	#elif 0
-	/*
-		This version is 49,0.75,0.32,0.33 ns per multiply-add for size = 1,10,100,1000.
-	*/
-	double alpha = 1.0, beta = 0.0;
-	NUMblas_dgemm ("N", "N", & target.nrow, & target.ncol, & x.ncol, & alpha,
-		(double *) & x [1] [1], & x.nrow, (double *) & y [1] [1], & y.nrow, & beta, & target [1] [1], & target.nrow);
-	#elif 0
-	/*
-		This version is 34,0.72,0.31,0.41(NoTrans;1.34Trans) ns per multiply-add for size = 1,10,100,1000.
-	*/
-	gsl_matrix gslx { (size_t) x.nrow, (size_t) x.ncol, (size_t) x.nrow, (double *) & x [1] [1], nullptr, false };
-	gsl_matrix gsly { (size_t) y.nrow, (size_t) y.ncol, (size_t) y.nrow, (double *) & y [1] [1], nullptr, false };
-	gsl_matrix gsltarget { (size_t) target.nrow, (size_t) target.ncol, (size_t) target.nrow, (double *) & target [1] [1], nullptr, false };
-	gsl_blas_dgemm (CblasTrans, CblasTrans, 1.0, & gslx, & gsly, 0.0, & gsltarget);
+				target [irow] [icol] = 0.0;
+			for (integer i = 1; i <= x.ncol; i ++)
+				for (integer icol = 1; icol <= target.ncol; icol ++)
+					target [irow] [icol] += x [irow] [i] * y [i] [icol];
+		}
 	#else
-	/*
-		This version is 20,0.80,0.32,0.33 ns per multiply-add for size = 1,10,100,1000.
-	*/
-	double *ptarget = & asvector (target) [1];
-	const double *px = & asvector (x) [1], *py = & asvector (y) [1];
-	for (integer irow = 0; irow < target.nrow; irow ++) {
-		for (integer icol = 0; icol < target.ncol; icol ++)
-			ptarget [irow * target.ncol + icol] = 0.0;
-		for (integer i = 0; i < x.ncol; i ++)
+		/*
+			This version is 20,0.80,0.32,0.33 ns per multiply-add for size = 1,10,100,1000.
+		*/
+		double *ptarget = & asvector (target) [1];
+		const double *px = & asvector (x) [1], *py = & asvector (y) [1];
+		for (integer irow = 0; irow < target.nrow; irow ++) {
 			for (integer icol = 0; icol < target.ncol; icol ++)
-				ptarget [irow * target.ncol + icol] += px [irow * x.ncol + i] * py [i * y.ncol + icol];
-	}
+				ptarget [irow * target.ncol + icol] = 0.0;
+			for (integer i = 0; i < x.ncol; i ++)
+				for (integer icol = 0; icol < target.ncol; icol ++)
+					ptarget [irow * target.ncol + icol] += px [irow * x.ncol + i] * py [i * y.ncol + icol];
+		}
 	#endif
 }
 
