37dfacdfdf1b3ed4e990af24f0b49cd122aa11c2
PaulBoersma <paul.boersma@uva.nl>
2019-09-25 23:54:20 +0200

    refactor FunctionEditor

--- fon/FunctionEditor.cpp
+++ fon/FunctionEditor.cpp
@@ -43,20 +43,20 @@ Thing_implement (FunctionEditor, Editor, 0);
 #include "FunctionEditor_prefs.h"
 
 namespace {
-	constexpr int maxGroup { 100 };
-	int nGroup = 0;
-	FunctionEditor theGroup [1 + maxGroup];
+	constexpr integer THE_MAXIMUM_GROUP_SIZE = 100;
+	integer theGroupSize = 0;
+	FunctionEditor theGroupMembers [1 + THE_MAXIMUM_GROUP_SIZE];
 }
 
 static void drawWhileDragging (FunctionEditor me, double x1, double x2);
 
-static int group_equalDomain (double tmin, double tmax) {
-	if (nGroup == 0)
-		return 1;
-	for (int i = 1; i <= maxGroup; i ++)
-		if (theGroup [i])
-			return tmin == theGroup [i] -> tmin && tmax == theGroup [i] -> tmax;
-	return 0;   // should not occur
+static bool group_equalDomain (double tmin, double tmax) {
+	if (theGroupSize == 0)
+		return true;
+	for (integer i = 1; i <= THE_MAXIMUM_GROUP_SIZE; i ++)
+		if (theGroupMembers [i])
+			return ( tmin == theGroupMembers [i] -> tmin && tmax == theGroupMembers [i] -> tmax );
+	return false;   // should not occur
 }
 
 static void updateScrollBar (FunctionEditor me) {
@@ -69,18 +69,21 @@ static void updateScrollBar (FunctionEditor me) {
 }
 
 static void updateGroup (FunctionEditor me) {
-	if (! my group) return;
-	for (int i = 1; i <= maxGroup; i ++) if (theGroup [i] && theGroup [i] != me) {
-		FunctionEditor thee = theGroup [i];
-		if (my pref_synchronizedZoomAndScroll ()) {
-			thy startWindow = my startWindow;
-			thy endWindow = my endWindow;
+	if (! my group)
+		return;
+	for (integer i = 1; i <= THE_MAXIMUM_GROUP_SIZE; i ++) {
+		if (theGroupMembers [i] && theGroupMembers [i] != me) {
+			FunctionEditor thee = theGroupMembers [i];
+			if (my pref_synchronizedZoomAndScroll ()) {
+				thy startWindow = my startWindow;
+				thy endWindow = my endWindow;
+			}
+			thy startSelection = my startSelection;
+			thy endSelection = my endSelection;
+			FunctionEditor_updateText (thee);
+			updateScrollBar (thee);
+			Graphics_updateWs (thy graphics.get());
 		}
-		thy startSelection = my startSelection;
-		thy endSelection = my endSelection;
-		FunctionEditor_updateText (thee);
-		updateScrollBar (thee);
-		Graphics_updateWs (thy graphics.get());
 	}
 }
 
@@ -111,7 +114,7 @@ static void drawNow (FunctionEditor me) {
 		Update rectangles.
 	*/
 
-	for (int i = 0; i < 8; i++)
+	for (integer i = 0; i < 8; i++)
 		my rect [i]. left = my rect [i]. right = 0;
 
 	/*
@@ -155,7 +158,7 @@ static void drawNow (FunctionEditor me) {
 	*/
 	if (my numberOfMarkers > 1) {
 		const double window = my endWindow - my startWindow;
-		for (int i = 1; i <= my numberOfMarkers; i ++) {
+		for (integer i = 1; i <= my numberOfMarkers; i ++) {
 			my rect [3 + i]. left = i == 1 ? my functionViewerLeft + MARGIN : my functionViewerLeft + MARGIN + (my functionViewerRight - my functionViewerLeft - MARGIN * 2) *
 				(my marker [i - 1] - my startWindow) / window;
 			my rect [3 + i]. right = my functionViewerLeft + MARGIN + (my functionViewerRight - my functionViewerLeft - MARGIN * 2) *
@@ -210,7 +213,7 @@ static void drawNow (FunctionEditor me) {
 	Graphics_setViewport (my graphics.get(), my functionViewerLeft, my functionViewerRight, 0.0, my height);
 	Graphics_setWindow (my graphics.get(), my functionViewerLeft, my functionViewerRight, 0.0, my height);
 	Graphics_setTextAlignment (my graphics.get(), Graphics_CENTRE, Graphics_HALF);
-	for (int i = 0; i < 8; i ++) {
+	for (integer i = 0; i < 8; i ++) {
 		const double left = my rect [i]. left, right = my rect [i]. right;
 		if (left < right)
 			Graphics_button (my graphics.get(), left, right, my rect [i]. bottom, my rect [i]. top);
@@ -219,7 +222,7 @@ static void drawNow (FunctionEditor me) {
 	#ifdef _WIN32
 		verticalCorrection *= 1.5;
 	#endif
-	for (int i = 0; i < 8; i ++) {
+	for (integer i = 0; i < 8; i ++) {
 		const double left = my rect [i]. left, right = my rect [i]. right;
 		const double bottom = my rect [i]. bottom, top = my rect [i]. top;
 		if (left < right) {
@@ -371,13 +374,13 @@ static void drawNow (FunctionEditor me) {
 void structFunctionEditor :: v_destroy () noexcept {
 	MelderAudio_stopPlaying (MelderAudio_IMPLICIT);
 	if (our group) {   // undangle
-		int i = 1;
-		while (theGroup [i] != this) {
-			Melder_assert (i < maxGroup);
+		integer i = 1;
+		while (theGroupMembers [i] != this) {
+			Melder_assert (i < THE_MAXIMUM_GROUP_SIZE);
 			i ++;
 		}
-		theGroup [i] = nullptr;
-		nGroup --;
+		theGroupMembers [i] = nullptr;
+		theGroupSize --;
 	}
 	FunctionEditor_Parent :: v_destroy ();
 }
@@ -397,7 +400,8 @@ void structFunctionEditor :: v_info () {
 /********** FILE MENU **********/
 
 static void gui_drawingarea_cb_resize (FunctionEditor me, GuiDrawingArea_ResizeEvent event) {
-	if (! my graphics) return;   // could be the case in the very beginning
+	if (! my graphics)
+		return;   // could be the case in the very beginning
 	Graphics_setWsViewport (my graphics.get(), 0, event -> width, 0, event -> height);
 	int width = event -> width + 21;
 	/*
@@ -860,7 +864,7 @@ static void menu_cb_moveEndOfSelectionBy (FunctionEditor me, EDITOR_ARGS_FORM) {
 }
 
 void FunctionEditor_shift (FunctionEditor me, double shift, bool needsUpdateGroup) {
-	double windowLength = my endWindow - my startWindow;
+	const double windowLength = my endWindow - my startWindow;
 	MelderAudio_stopPlaying (MelderAudio_IMPLICIT);   // quickly, before window changes
 	trace (U"shifting by ", shift);
 	if (shift < 0.0) {
@@ -960,7 +964,8 @@ static void menu_cb_moveEndOfSelectionRight (FunctionEditor me, EDITOR_ARGS_DIRE
 /********** GUI CALLBACKS **********/
 
 static void gui_cb_scroll (FunctionEditor me, GuiScrollBarEvent event) {
-	if (! my graphics) return;   // ignore events during creation
+	if (! my graphics)
+		return;   // ignore events during creation
 	const double value = GuiScrollBar_getValue (event -> scrollBar);
 	const double shift = my tmin + (value - 1) * (my tmax - my tmin) / maximumScrollBarValue - my startWindow;
 	const bool shifted = ( shift != 0.0 );
@@ -995,38 +1000,40 @@ static void gui_cb_scroll (FunctionEditor me, GuiScrollBarEvent event) {
 		#endif
 		if (! my group || ! my pref_synchronizedZoomAndScroll ())
 			return;
-		for (int i = 1; i <= maxGroup; i ++) if (theGroup [i] && theGroup [i] != me) {
-			theGroup [i] -> startWindow = my startWindow;
-			theGroup [i] -> endWindow = my endWindow;
-			FunctionEditor_updateText (theGroup [i]);
-			updateScrollBar (theGroup [i]);
-			#if cocoa
-				Graphics_updateWs (theGroup [i] -> graphics.get());
-			#else
-				Graphics_clearWs (theGroup [i] -> graphics.get());
-				drawNow (theGroup [i]);
-			#endif
+		for (integer i = 1; i <= THE_MAXIMUM_GROUP_SIZE; i ++) {
+			if (theGroupMembers [i] && theGroupMembers [i] != me) {
+				theGroupMembers [i] -> startWindow = my startWindow;
+				theGroupMembers [i] -> endWindow = my endWindow;
+				FunctionEditor_updateText (theGroupMembers [i]);
+				updateScrollBar (theGroupMembers [i]);
+				#if cocoa
+					Graphics_updateWs (theGroupMembers [i] -> graphics.get());
+				#else
+					Graphics_clearWs (theGroupMembers [i] -> graphics.get());
+					drawNow (theGroupMembers [i]);
+				#endif
+			}
 		}
 	}
 }
 
 static void gui_checkbutton_cb_group (FunctionEditor me, GuiCheckButtonEvent /* event */) {
-	int i;
+	integer i;
 	my group = ! my group;
 	if (my group) {
 		FunctionEditor thee;
 		i = 1;
-		while (theGroup [i])
+		while (theGroupMembers [i])
 			i ++;
-		theGroup [i] = me;
-		if (++ nGroup == 1) {
+		theGroupMembers [i] = me;
+		if (++ theGroupSize == 1) {
 			Graphics_updateWs (my graphics.get());
 			return;
 		}
 		i = 1;
-		while (! theGroup [i] || theGroup [i] == me)
+		while (! theGroupMembers [i] || theGroupMembers [i] == me)
 			i ++;
-		thee = theGroup [i];
+		thee = theGroupMembers [i];
 		if (my pref_synchronizedZoomAndScroll ()) {
 			my startWindow = thy startWindow;
 			my endWindow = thy endWindow;
@@ -1045,23 +1052,26 @@ static void gui_checkbutton_cb_group (FunctionEditor me, GuiCheckButtonEvent /* 
 			my v_updateText ();
 			updateScrollBar (me);
 			Graphics_updateWs (my graphics.get());
-			if (my tmin < thy tmin || my tmax > thy tmax)
-				for (i = 1; i <= maxGroup; i ++) if (theGroup [i] && theGroup [i] != me) {
-					if (my tmin < thy tmin)
-						theGroup [i] -> tmin = my tmin;
-					if (my tmax > thy tmax)
-						theGroup [i] -> tmax = my tmax;
-					FunctionEditor_updateText (theGroup [i]);
-					updateScrollBar (theGroup [i]);
-					Graphics_updateWs (theGroup [i] -> graphics.get());
+			if (my tmin < thy tmin || my tmax > thy tmax) {
+				for (i = 1; i <= THE_MAXIMUM_GROUP_SIZE; i ++) {
+					if (theGroupMembers [i] && theGroupMembers [i] != me) {
+						if (my tmin < thy tmin)
+							theGroupMembers [i] -> tmin = my tmin;
+						if (my tmax > thy tmax)
+							theGroupMembers [i] -> tmax = my tmax;
+						FunctionEditor_updateText (theGroupMembers [i]);
+						updateScrollBar (theGroupMembers [i]);
+						Graphics_updateWs (theGroupMembers [i] -> graphics.get());
+					}
 				}
+			}
 		}
 	} else {
 		i = 1;
-		while (theGroup [i] != me)
+		while (theGroupMembers [i] != me)
 			i ++;
-		theGroup [i] = nullptr;
-		nGroup --;
+		theGroupMembers [i] = nullptr;
+		theGroupSize --;
 		my v_updateText ();
 		Graphics_updateWs (my graphics.get());   // for setting buttons in draw method
 	}
@@ -1178,8 +1188,10 @@ static void gui_drawingarea_cb_click (FunctionEditor me, GuiDrawingArea_ClickEve
 			BOTTOM_MARGIN + space * 3, my height - (TOP_MARGIN + space));
 		Graphics_setWindow (my graphics.get(), my startWindow, my endWindow, 0.0, 1.0);
 		Graphics_DCtoWC (my graphics.get(), event -> x, event -> y, & xWC, & yWC);
-		if (xWC < my startWindow) xWC = my startWindow;
-		if (xWC > my endWindow) xWC = my endWindow;
+		if (xWC < my startWindow)
+			xWC = my startWindow;
+		if (xWC > my endWindow)
+			xWC = my endWindow;
 		if (Melder_debug == 24) {
 			Melder_casual (U"FunctionEditor::gui_drawingarea_cb_click:"
 				U" button ", event -> button,
@@ -1218,7 +1230,8 @@ static void gui_drawingarea_cb_click (FunctionEditor me, GuiDrawingArea_ClickEve
 		try {
 			for (int i = 0; i < 8; i ++) {
 				if (xWC > my rect [i]. left && xWC < my rect [i]. right &&
-					 yWC > my rect [i]. bottom && yWC < my rect [i]. top)
+						yWC > my rect [i]. bottom && yWC < my rect [i]. top)
+				{
 					switch (i) {
 						case 0: my v_play (my tmin, my tmax); break;
 						case 1: my v_play (my startWindow, my endWindow); break;
@@ -1229,6 +1242,7 @@ static void gui_drawingarea_cb_click (FunctionEditor me, GuiDrawingArea_ClickEve
 						case 6: my v_play (my marker [2], my marker [3]); break;
 						case 7: my v_play (my startSelection, my endSelection); break;
 					}
+				}
 			}
 		} catch (MelderError) {
 			Melder_flushError ();
@@ -1571,21 +1585,15 @@ bool structFunctionEditor :: v_click (double xbegin, double ybegin, bool a_shift
 
 bool structFunctionEditor :: v_clickB (double xWC, double /* yWC */) {
 	our startSelection = xWC;
-	if (our startSelection > our endSelection) {
-		double dummy = our startSelection;
-		our startSelection = our endSelection;
-		our endSelection = dummy;
-	}
+	if (our startSelection > our endSelection)
+		std::swap (our startSelection, our endSelection);
 	return FunctionEditor_UPDATE_NEEDED;
 }
 
 bool structFunctionEditor :: v_clickE (double xWC, double /* yWC */) {
 	our endSelection = xWC;
-	if (our startSelection > our endSelection) {
-		double dummy = our startSelection;
-		our startSelection = our endSelection;
-		our endSelection = dummy;
-	}
+	if (our startSelection > our endSelection)
+		std::swap (our startSelection, our endSelection);
 	return FunctionEditor_UPDATE_NEEDED;
 }
 
@@ -1726,13 +1734,15 @@ void FunctionEditor_enableUpdates (FunctionEditor me, bool enable) {
 }
 
 void FunctionEditor_ungroup (FunctionEditor me) {
-	if (! my group) return;
+	if (! my group)
+		return;
 	my group = false;
 	GuiCheckButton_setValue (my groupButton, false);
-	int i = 1;
-	while (theGroup [i] != me) i ++;
-	theGroup [i] = nullptr;
-	nGroup --;
+	integer i = 1;
+	while (theGroupMembers [i] != me)
+		i ++;
+	theGroupMembers [i] = nullptr;
+	theGroupSize --;
 	my v_updateText ();
 	Graphics_updateWs (my graphics.get());   // for setting buttons in v_draw() method
 }
