7adc4fa78ba9f3bc14c6646b47cb42c489f9ca77
David Weenink <david.weenink@uva.nl>
2020-02-25 23:38:41 +0100

    rid many NUMvector

--- sys/oo.h
+++ sys/oo.h
@@ -103,6 +103,7 @@
 #define oo_INTVEC16(x,size)  oo_ANYVEC (integer, integer16BE, x, size)
 #define oo_BOOLVEC(x,size)  oo_ANYVEC (bool, eb, x, size)
 #define oo_COMPVEC(x,size) oo_ANYVEC (dcomplex, c128, x, size)
+#define oo_BYTEVEC(x,size) oo_ANYVEC (byte, u8, x, size)
 
 #define oo_MAT(x,nrow,ncol)  oo_ANYMAT (double, r64, x, nrow, ncol)
 #define oo_INTMAT(x,nrow,ncol)  oo_ANYMAT (integer, integer32BE, x, nrow, ncol)
