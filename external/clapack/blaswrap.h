46d531922f8dcce3774bb5de05a8930799d43b1a
David Weenink <david.weenink@uva.nl>
2020-03-21 21:58:40 +0100

    clapack README

--- external/clapack/blaswrap.h
+++
@@ -1,9 +0,0 @@
-/* CLAPACK 3.0 BLAS wrapper macros
- * Feb 5, 2000
- * 20200319 David Weenink, empty because we don't need it.
- */
-
-#ifndef __BLASWRAP_H
-#define __BLASWRAP_H
-
-#endif /* __BLASWRAP_H */
