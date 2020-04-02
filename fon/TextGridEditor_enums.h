8eae160edb5e5f5f4aa735b6af8a202b364b9a14
PaulBoersma <paul.boersma@uva.nl>
2020-04-02 15:03:16 +0200

    rid some unreachable code

--- fon/TextGridEditor_enums.h
+++ fon/TextGridEditor_enums.h
@@ -1,6 +1,6 @@
 /* TextGridEditor_enums.h
  *
- * Copyright (C) 1992-2007,2013,2015 Paul Boersma
+ * Copyright (C) 1992-2005,2007,2013,2015,2016,2020 Paul Boersma
  *
  * This code is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,33 +21,5 @@ enums_begin (kTextGridEditor_showNumberOf, 1)
 	enums_add (kTextGridEditor_showNumberOf, 2, INTERVALS_OR_POINTS, U"intervals or points")
 	enums_add (kTextGridEditor_showNumberOf, 3, NONEMPTY_INTERVALS_OR_POINTS, U"non-empty intervals or points")
 enums_end (kTextGridEditor_showNumberOf, 3, INTERVALS_OR_POINTS)
-
-enums_begin (kTextGridEditor_language, 1)
-	enums_add (kTextGridEditor_language, 1, AFRIKAANS, U"Afrikaans")
-	enums_add (kTextGridEditor_language, 2, AKAN, U"Akan")
-	enums_add (kTextGridEditor_language, 3, ALBANIAN, U"Albanian")
-	enums_add (kTextGridEditor_language, 4, AMHARIC, U"Amharic")
-	enums_add (kTextGridEditor_language, 5, ARMENIAN, U"Armenian")
-	enums_add (kTextGridEditor_language, 6, ARMENIAN_WEST, U"Armenian (West)")
-	enums_add (kTextGridEditor_language, 7, AZERBAIJANI, U"Azerbaijani")
-	enums_add (kTextGridEditor_language, 8, BOSNIAN, U"Bosnian")
-	enums_add (kTextGridEditor_language, 9, BULGARIAN, U"Bulgarian")
-	enums_add (kTextGridEditor_language, 10, CANTONESE, U"Cantonese")
-	enums_add (kTextGridEditor_language, 11, CATALAN, U"Catalan")
-	enums_add (kTextGridEditor_language, 12, CROATIAN, U"Croatian")
-	enums_add (kTextGridEditor_language, 13, CZECH, U"Czech")
-	enums_add (kTextGridEditor_language, 14, DARI, U"Danish")
-	enums_add (kTextGridEditor_language, 15, DEFAULT_LANGUAGE, U"Default language")
-	enums_add (kTextGridEditor_language, 16, DIVEHI, U"Divehi")
-	enums_add (kTextGridEditor_language, 17, DUTCH, U"Dutch-test")
-	enums_add (kTextGridEditor_language, 18, ENGLISH_AMERICAN, U"English (American)")
-	enums_add (kTextGridEditor_language, 19, ENGLISH_RP, U"English (RP)")
-	enums_add (kTextGridEditor_language, 20, ENGLISH_SCOTLAND, U"English (Scotland)")
-	enums_add (kTextGridEditor_language, 21, ENGLISH_SOUTHERN_ENGLAND, U"English (Southern England)")
-	enums_add (kTextGridEditor_language, 22, ENGLISH_WEST_INDIES, U"English (West Indies)")
-	enums_add (kTextGridEditor_language, 22, ENGLISH_WEST_MIDLANDS, U"English (West Midlands)")
-	enums_add (kTextGridEditor_language, 23, PORTUGUESE_BRAZILIAN, U"Portuguese (Brazilian)")
-	enums_add (kTextGridEditor_language, 24, PORTUGUESE_EUROPEAN, U"Portuguese (European)")
-enums_end (kTextGridEditor_language, 2, ENGLISH_AMERICAN)
 
 /* End of file TextGridEditor_enums.h */
