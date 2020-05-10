f12bf2e6a2d834bd18855d1f8bebac6ea85d74e0
David Weenink <david.weenink@uva.nl>
2020-05-10 15:02:47 +0200

    FormantEditor needs three or four objects

--- dwtools/praat_DataModeler_init.cpp
+++ dwtools/praat_DataModeler_init.cpp
@@ -442,15 +442,26 @@ static void cb_FormantEditor_publication (Editor /* editor */, autoDaata publica
 	}
 }
 
-DIRECT (WINDOW_FormantEditor_viewAndEdit) {
+DIRECT (WINDOW_FormantEditor_viewAndEdit4) {
 	if (theCurrentPraatApplication -> batch)
 		Melder_throw (U"Cannot view or edit a Formant from batch.");
-	FIND_THREE_WITH_IOBJECT (FormantList, Sound, TextGrid)
-		autoFormantEditor editor = FormantEditor_create (ID_AND_FULL_NAME, me, you, true, him, nullptr);
+	FIND_FOUR_WITH_IOBJECT (Formant, FormantList, Sound, TextGrid)
+		autoFormantEditor editor = FormantEditor_create (ID_AND_FULL_NAME, me, you, him, true, she, nullptr);
 		Editor_setPublicationCallback (editor.get(), cb_FormantEditor_publication);
 		praat_installEditor (editor.get(), IOBJECT);
 		editor.releaseToUser();
 	END
+}
+DIRECT (WINDOW_FormantEditor_viewAndEdit3) {
+	if (theCurrentPraatApplication -> batch)
+		Melder_throw (U"Cannot view or edit a Formant from batch.");
+	FIND_THREE_WITH_IOBJECT (Formant, FormantList, Sound)
+		autoFormantEditor editor = FormantEditor_create (ID_AND_FULL_NAME, me, you, him, true, nullptr, nullptr);
+		Editor_setPublicationCallback (editor.get(), cb_FormantEditor_publication);
+		praat_installEditor (editor.get(), IOBJECT);
+		editor.releaseToUser();
+	END
+	
 }
 
 /********************** FormantModeler ******************************/
@@ -1237,7 +1248,8 @@ void praat_DataModeler_init () {
 	praat_addAction1 (classSound, 0, U"To Formant (interval, constrained, robust)...", U"To Formant (interval, constrained)...", 
 		praat_DEPTH_2 | praat_HIDDEN, NEW_Sound_to_Formant_interval_constrained_robust);
 	praat_addAction1 (classTable, 0, U"To DataModeler...", U"To logistic regression...", praat_DEPTH_1 + praat_HIDDEN, NEW_Table_to_DataModeler);
-	praat_addAction3 (classFormantList, 1, classSound, 1, classTextGrid, 1,  U"View & Edit...", nullptr, praat_ATTRACTIVE, WINDOW_FormantEditor_viewAndEdit);
+	praat_addAction3 (classFormant, 1, classFormantList, 1, classSound, 1, U"View & Edit...", nullptr, praat_ATTRACTIVE, WINDOW_FormantEditor_viewAndEdit3);
+	praat_addAction4 (classFormant, 1, classFormantList, 1, classSound, 1, classTextGrid, 1,  U"View & Edit...", nullptr, praat_ATTRACTIVE, WINDOW_FormantEditor_viewAndEdit4);
 }
 
 /* End of file praat_DataModeler_init.cpp 1566*/
