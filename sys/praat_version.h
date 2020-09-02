8fbe5a5646686dc65c3c1280c9d30856ffb1030a
PaulBoersma <paul.boersma@uva.nl>
2020-09-02 04:26:27 +0200

    (unstable:) for version 6.1.18

--- sys/praat_version.h
+++ sys/praat_version.h
@@ -1,5 +1,5 @@
-#define PRAAT_VERSION_STR 6.1.17alpha
-#define PRAAT_VERSION_NUM 6117
+#define PRAAT_VERSION_STR 6.1.18alpha
+#define PRAAT_VERSION_NUM 6118
 #define PRAAT_YEAR 2020
-#define PRAAT_MONTH August
-#define PRAAT_DAY 16
+#define PRAAT_MONTH September
+#define PRAAT_DAY 1
