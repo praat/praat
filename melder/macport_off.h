f1e8e4e5935f499f540d08ad76ca6f7aa8a396f5
PaulBoersma <paul.boersma@uva.nl>
2021-01-16 00:05:41 +0100

    move macport_on.off to where winport_on/off is

---
+++ melder/macport_off.h
@@ -0,0 +1,25 @@
+/* macport_off.h
+ *
+ * Copyright (C) 1993-2011 Paul Boersma
+ *
+ * This code is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This code is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this work. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#undef Polygon
+#undef Pattern
+#undef Collection
+#undef Picture
+#undef Movie
+
+/* End of file macport_off.h */
