e328b1b6a2eb8108ab2e91ce71b62402128853e8
PaulBoersma <paul.boersma@uva.nl>
2021-03-28 22:34:41 +0200

    for version 6.1.41

--- sys/praat_version.h
+++ sys/praat_version.h
@@ -1,5 +1,5 @@
-#define PRAAT_VERSION_STR 6.1.40
-#define PRAAT_VERSION_NUM 6140
+#define PRAAT_VERSION_STR 6.1.41
+#define PRAAT_VERSION_NUM 6141
 #define PRAAT_YEAR 2021
-#define PRAAT_MONTH February
-#define PRAAT_DAY 27
+#define PRAAT_MONTH March
+#define PRAAT_DAY 25
