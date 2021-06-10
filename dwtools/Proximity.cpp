6bec7da71cec278336477dc324ca053a6425ee65
David Weenink <david.weenink@uva.nl>
2021-06-10 17:33:59 +0200

    improve additive constant calculation

--- dwtools/Proximity.cpp
+++ dwtools/Proximity.cpp
@@ -38,19 +38,13 @@ void Proximity_init (Proximity me, integer numberOfPoints) {
 
 Thing_implement (Dissimilarity, Proximity, 0);
 
-static double Dissimilarity_getAverage (Dissimilarity me) {
-	longdouble sum = 0.0;
-	integer numberOfPositives = 0;
-	for (integer i = 1; i <= my numberOfRows - 1; i ++) {
-		for (integer j = i + 1; j <= my numberOfRows; j ++) {
-			const longdouble proximity = 0.5 * (my data [i] [j] + my data [j] [i]);
-			if (proximity > 0.0) {
-				numberOfPositives ++;
-				sum += proximity;
-			}
-		}
-	}
-	return numberOfPositives > 0 ? (double) sum / numberOfPositives : undefined;
+static bool Dissimilarity_hasNoNegativeValues (Dissimilarity me) {
+	bool noNegatives = true;
+	for (integer i = 1; i <= my numberOfRows - 1; i ++)
+		for (integer j = i + 1; j <= my numberOfRows; j ++)
+			if (my data [i] [j] < 0.0)
+				return false;
+	return true;
 }
 
 autoDissimilarity Dissimilarity_create (integer numberOfPoints) {
@@ -88,17 +82,13 @@ autoDissimilarity Dissimilarity_createLetterRExample (double noiseStd) {
 	F. Cailliez (1983), The analytical solution of the additive constant problem, Psychometrika 48, 305-308.
 */
 double Dissimilarity_getAdditiveConstant (Dissimilarity me) {
-	double additiveConstant = undefined;
 	try {
 		const integer nPoints = my numberOfRows, nPoints2 = 2 * nPoints;
 		Melder_require (nPoints > 0,
 			U"Matrix part should not be empty.");
-		/*
-			Return c = average dissimilarity in case of failure
-		*/
-		additiveConstant = Dissimilarity_getAverage (me);
-		Melder_require (isdefined (additiveConstant),
-			U"There are no positive dissimilarities.");
+
+		Melder_require (Dissimilarity_hasNoNegativeValues (me),
+			U"Dissimilarities should not be negative.");
 		
 		autoMAT wd = zero_MAT (nPoints, nPoints);
 		autoMAT wdsqrt = zero_MAT (nPoints, nPoints);
@@ -115,6 +105,19 @@ double Dissimilarity_getAdditiveConstant (Dissimilarity me) {
 
 		doubleCentre_MAT_inout (wdsqrt.get());
 		doubleCentre_MAT_inout (wd.get());
+		
+		/*
+			if wd is not positive semi-definite then the dissimilarities have no euclidean representation
+			and we need to calculate an additive constant c such that the dissimilarities 
+			d'[i][j] = d [i][j] + c have an euclidean representation.
+			A positive semi-definite matrix has all eigenvalues larger than or equal to zero.
+		*/
+		autoVEC eigenvalues_wd;
+		MAT_getEigenSystemFromSymmetricMatrix (wd.get(), nullptr, & eigenvalues_wd, true);
+		
+		if (eigenvalues_wd [1] >= 0.0)
+			return 0.0;
+		
 		/*
 			Calculate the B matrix according to eq. 6
 		*/
@@ -123,28 +126,20 @@ double Dissimilarity_getAdditiveConstant (Dissimilarity me) {
 		b.part (nPoints + 1, nPoints2, 1, nPoints).diagonal()  <<=  -1.0;
 		b.part (nPoints + 1, nPoints2, nPoints + 1, nPoints2)  <<=  -4.0  *  wdsqrt.get();
 		/*
-			Get eigenvalues
+			Get eigenvalues of B
 		*/
 		autoCOMPVEC eigenvalues;
 		MAT_getEigenSystemFromGeneralSquareMatrix (b.get(), & eigenvalues, nullptr);
 		/*
-			Get largest real eigenvalue
+			Get largest real eigenvalue. This value needs to be larger than or equal to zero,
+			therefore we left-clip against zero. 
+			Even if all eigenvalues turn out to be negative we set it to zero.
 		*/
-		integer numberOfRealEigenvalues = 0;
-		double largestRealEigenvalue = std::numeric_limits<double>::lowest();
-		for (integer i = 1; i <= nPoints2; i ++) {
-			if (eigenvalues [i] .imag() == 0.0) {
-				++ numberOfRealEigenvalues;
-				if (eigenvalues [i] .real() > largestRealEigenvalue)
-					largestRealEigenvalue = eigenvalues [i].real();
-			}
-		}
-		
-		Melder_require (largestRealEigenvalue >= 0,
-			U"The largest eigenvalue should be positive.");
-		
-		additiveConstant = largestRealEigenvalue;
-		return additiveConstant;
+		double largestRealEigenvalue = 0.0;
+		for (integer i = 1; i <= nPoints2; i ++)
+			if (eigenvalues [i].imag() == 0.0 && eigenvalues [i].real() > largestRealEigenvalue)
+				largestRealEigenvalue = eigenvalues [i].real();
+		return largestRealEigenvalue;
 	} catch (MelderError) {
 		Melder_throw (U"Additive constant not calculated.");
 	}
