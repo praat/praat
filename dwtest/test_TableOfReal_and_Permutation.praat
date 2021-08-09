e5f9736458b8f4c1b4644e7a21b524694d93ad0e
David Weenink <david.weenink@uva.nl>
2021-08-09 20:53:19 +0200

    test TableOfReal_and_Permutation

--- dwtest/test_TableOfReal_and_Permutation.praat
+++ dwtest/test_TableOfReal_and_Permutation.praat
@@ -1,51 +1,34 @@
 # test_TableOfReal_and_Permutation.praat
-# djmw 20050713, 20070829
+# djmw 20210809
 
-nrows = 30
-Create TableOfReal... test nrows 2
-testtab = selected ("TableOfReal")
-Formula... row*col
-labels$ = "aabbccddeeffgghhiijjk"
+appendInfoLine: tab$, "test_TableOfReal_and_Permutation.praat"
 
+numberOfRows = 100
+numberOfColumns = 10
 
-printline test: TableOfReal_and_Permutation.praat
-for itry to 10
-  select testtab
-  for i to nrows
-    randomIndex = randomInteger (1,20)
-    label$ = mid$ (labels$, randomIndex, 1)
-    Set row label (index)... i 'label$'
-  endfor
-  To Permutation (sort row labels)
-  pid = selected ("Permutation")
-  plus testtab
-  Permute rows
-  testtabp = selected ("TableOfReal")
-  label$ = Get row label... 1
-  for i from 2 to nrows  
-    lab$ = Get row label... i
-    assert lab$ >= label$ ; i='i' 'lab$' > 'label$'
-    label$ = lab$
-  endfor
-  select pid
-  Invert
-  pidi = selected ("Permutation")
-  plus testtabp
-  Permute rows
-  testtabpi = selected ("TableOfReal")
-  Formula... self - TableOfReal_test[]
-  stdev = Get column stdev (index)... 1
-  assert stdev = 0
-  select pid
-  plus testtabp
-  plus pidi
-  plus testtabpi
-  Remove
-  print .
+p_rows = Create Permutation: "pr", numberOfRows, "no"
+p_rows# = List values
+p_cols = Create Permutation: "pc", numberOfColumns, "no"
+p_cols# = List values
+
+tor = Create TableOfReal: "t", numberOfRows, numberOfColumns
+Formula: "randomUniform (0,1)"
+
+selectObject: p_rows, tor
+tor_rows = Permute rows
+for irow to numberOfRows
+	for icol to numberOfColumns
+		assert object[tor_rows, irow, icol] = object [tor, p_rows# [irow] , icol]
+	endfor
 endfor
-printline o.k.
-select testtab
-Remove
+selectObject: p_cols, tor
+tor_cols = Permute columns
+for icol to numberOfColumns
+	for irow to numberOfRows
+		assert object[tor_cols, irow,icol] = object [tor, irow , p_cols#[icol]]
+	endfor
+endfor
+removeObject: tor_cols, tor_rows, tor, p_cols, p_rows
 
-
+appendInfoLine: tab$, "test_TableOfReal_and_Permutation.praat OK"
 
--- dwtools/TableOfReal_and_Permutation.cpp
+++ dwtools/TableOfReal_and_Permutation.cpp
@@ -1,6 +1,6 @@
 /* TableOfReal_and_Permutation.cpp
  *
- * Copyright (C) 2005-2018 David Weenink
+ * Copyright (C) 2005-2021 David Weenink
  *
  * This code is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -30,16 +30,36 @@ autoTableOfReal TableOfReal_Permutation_permuteRows (TableOfReal me, Permutation
 			U"The number of rows in the table and the number of elements in the Permutation should be equal.");
 		autoTableOfReal him = TableOfReal_create (my numberOfRows, my numberOfColumns);
 
-		for (integer i = 1; i <= thy numberOfElements; i ++)
-			TableOfReal_copyOneRowWithLabel (me, him.get(), thy p [i], i);
-		for (integer j = 1; j <= my numberOfColumns; j ++)
-			TableOfReal_setColumnLabel (him.get(), j, my columnLabels [j].get());
+		for (integer irow = 1; irow <= thy numberOfElements; irow ++)
+			TableOfReal_copyOneRowWithLabel (me, him.get(), thy p [irow], irow);
+		for (integer icol = 1; icol <= my numberOfColumns; icol ++)
+			TableOfReal_setColumnLabel (him.get(), icol, my columnLabels [icol].get());
 		return him;
 	} catch (MelderError) {
-		Melder_throw (me, U": not permuted.");
+		Melder_throw (me, U": rows not permuted.");
 	}
 }
 
+autoTableOfReal TableOfReal_Permutation_permuteColumns (TableOfReal me, Permutation thee) {
+	try {
+		Melder_require (my numberOfColumns == thy numberOfElements,
+			U"The number of columns in the table and the number of elements in the Permutation should be equal.");
+		autoTableOfReal him = TableOfReal_create (my numberOfRows, my numberOfColumns);
+
+		for (integer icol = 1; icol <= thy numberOfElements; icol ++) {
+			const integer fromCol = thy p [icol];
+			his columnLabels [icol] = Melder_dup (my columnLabels [fromCol].get());
+			for (integer irow = 1; irow <= my numberOfRows; irow ++)
+				his data [irow] [icol] = my data [irow][fromCol];
+		}
+		for (integer irow = 1; irow <= my numberOfColumns; irow ++)
+			TableOfReal_setRowLabel (him.get(), irow, my rowLabels [irow].get());
+		return him;
+	} catch (MelderError) {
+		Melder_throw (me, U": columns not permuted.");
+	}
+	
+}
 autoPermutation TableOfReal_to_Permutation_sortRowLabels (TableOfReal me) {
 	try {
 		autoPermutation thee = Permutation_create (my numberOfRows);
