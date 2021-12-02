e230c5adacf6a167885933a6cd50551a9a3125fe
David Weenink <david.weenink@uva.nl>
2021-12-02 22:10:00 +0100

    date

--- dwtools/PCA.cpp
+++ dwtools/PCA.cpp
@@ -2,7 +2,7 @@
  *
  * Principal Component Analysis
  *
- * Copyright (C) 1993-2019 David Weenink
+ * Copyright (C) 1993-2021 David Weenink
  *
  * This code is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
