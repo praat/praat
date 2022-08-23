fa117aff1feb50e16aa1b13c50a3628565617448
PaulBoersma <paul.boersma@uva.nl>
2022-08-23 21:59:26 +0200

    Windows mono as stereo

--- melder/melder_audio.cpp
+++ melder/melder_audio.cpp
@@ -1036,7 +1036,7 @@ void MelderAudio_play16 (int16 *buffer, integer sampleRate, integer numberOfSamp
 		}
 	#endif
 	my playBuffer = buffer;   // 0-based, as all buffers are
-	int16 *playBuffer2 = nullptr;   // in case we have to redistribute up (not ideal)
+	static int16 *playBuffer2 = nullptr;   // in case we have to redistribute up (not ideal)
 	my sampleRate = sampleRate;
 	my numberOfSamples = numberOfSamples;
 	my numberOfChannels = numberOfChannels;
@@ -1376,6 +1376,8 @@ void MelderAudio_play16 (int16 *buffer, integer sampleRate, integer numberOfSamp
 						}
 					}
 				} else if (numberOfChannels == 1 && my numberOfChannels == 2) {
+					if (playBuffer2)
+						Melder_free (playBuffer2);
 					playBuffer2 = Melder_malloc (int16, numberOfSamples * my numberOfChannels);
 					Melder_assert (playBuffer2);
 					int16 *in = & my playBuffer [0], *out = & playBuffer2 [0];
@@ -1384,11 +1386,11 @@ void MelderAudio_play16 (int16 *buffer, integer sampleRate, integer numberOfSamp
 						*out ++ = inValue;
 						*out ++ = inValue;
 					}
-					std::swap (my playBuffer, playBuffer2);
 				}
 
 				my waveHeader. dwFlags = 0;
-				my waveHeader. lpData = playBuffer2 ? (char *) playBuffer2 : (char *) my playBuffer;
+				my waveHeader. lpData =
+						( numberOfChannels == 1 && my numberOfChannels == 2 ? (char *) playBuffer2 : (char *) my playBuffer );
 				my waveHeader. dwBufferLength = my numberOfSamples * 2 * my numberOfChannels;
 				my waveHeader. dwLoops = 1;
 				my waveHeader. lpNext = nullptr;
