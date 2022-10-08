0f2c5bfbfd6497e121d260669fb1fc70a8470de7
PaulBoersma <paul.boersma@uva.nl>
2022-10-08 13:59:18 +0200

    for version 6.2.23

--- sys/praat_version.h
+++ sys/praat_version.h
@@ -1,5 +1,5 @@
-#define PRAAT_VERSION_STR 6.2.22
-#define PRAAT_VERSION_NUM 6222
+#define PRAAT_VERSION_STR 6.2.23
+#define PRAAT_VERSION_NUM 6223
 #define PRAAT_YEAR 2022
 #define PRAAT_MONTH October
-#define PRAAT_DAY 6
+#define PRAAT_DAY 8
