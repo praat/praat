1b5e759d7a891921ef7e6db585a8c2335c268042
davidweenink <david.weenink@uva.nl>
2022-10-12 00:20:18 +0200

    replace macros by templates

--- dwsys/NUM2.h
+++ dwsys/NUM2.h
@@ -23,6 +23,7 @@
 */
 
 #include <algorithm>
+#include "DigitstringNumber.h"
 #include <limits.h>
 #include "melder.h"
 #include "MAT_numerics.h"
@@ -298,8 +299,8 @@ autoVEC newVECmonotoneRegression (constVEC x);
 */
 
 
-/* NUMsort2:
-	NUMsort2 uses heapsort to sort the second array in parallel with the first one.
+/*
+	Uses heapsort to sort the second array in parallel with the first one.
 
 	Algorithm follows p. 145 and 642 in:
 	Donald E. Knuth (1998): The art of computer programming. Third edition. Vol. 3: sorting and searching.
@@ -316,23 +317,17 @@ autoVEC newVECmonotoneRegression (constVEC x);
 template<typename T1, typename T2>
 void NUMsortTogether (vector<T1> a, vector<T2> b) {
 	Melder_assert (a.size == b.size);
-	T1 k, min;
-	T2 kb, min2;
 	if (a.size < 2) return;   /* Already sorted. */
 	if (a.size == 2) {
-		if (a [1] > a [2]) {
-			min = a [2];
-			a [2] = a [1];
-			a [1] = min;
-			min2 = b [2];
-			b [2] = b [1];
-			b [1] = min2;
+		if (a [2] < a [1]) {
+			std::swap (a [1], a [2]);
+			std::swap (b [1], b [2]);
 		}
 		return;
 	}
 	if (a.size <= 12) {
 		for (integer i = 1; i < a.size; i ++) {
-			min = a [i];
+			T1 min = a [i];
 			integer imin = i;
 			for (integer j = i + 1; j <= a.size; j ++)
 				if (a [j] < min) {
@@ -341,16 +336,17 @@ void NUMsortTogether (vector<T1> a, vector<T2> b) {
 				}
 			a [imin] = a [i];
 			a [i] = min;
-			min2 = b [imin];
-			b [imin] = b [i];
-			b [i] = min2;
+			std::swap (b [imin], b [i]);
 		}
 		return;
 	}
-	/* H1 */
+	/* H1 Initialize */
 	integer l = (a.size >> 1) + 1;
 	integer r = a.size;
 	for (;;) {
+		T1	k;
+		T2 kb;
+		/* H2 Decrease */
 		if (l > 1) {
 			l --;
 			k = a [l];
@@ -397,27 +393,121 @@ void NUMsortTogether (vector<T1> a, vector<T2> b) {
 void VECsort3_inplace (VEC const& a, INTVEC const& iv1, INTVEC const& iv2, bool descending); // TODO template
 /* Sort a together with iv1  and iv2 */
 
-void INTVECindex (INTVEC const& target, constINTVEC const& a);
-void INTVECindex (INTVEC const& target, constVEC const& a);
-void INTVECindex (INTVEC const& target, constSTRVEC const& v);
+template <class T, typename Tt>
+void INTVECindex2_inout (INTVEC & index, T v, bool (*compare) (Tt const& e1, Tt const& e2)) {
+	Melder_assert (v.size == index.size);
+	to_INTVEC_out (index);
+	if (v.size < 2)
+		return;   /* Already sorted. */
+	if (v.size == 2) {
+		if (compare (v [2], v [1])) {
+			index [1] = 2;
+			index [2] = 1;
+		}
+		return;
+	}
+	
+	if (v.size <= 12) {
+		for (integer i = 1; i < v.size; i ++) {
+			integer imin = i;
+			Tt min = v [index [imin]];
+			for (integer j = i + 1; j <= v.size; j ++) {
+				if (compare (v [index [j]], min)) {
+					imin = j;
+					min = v [index [j]];
+				}
+			}
+			std::swap (index [imin], index [i]);
+		}
+		return;
+	}
+	/* H1 */
+	integer l = v.size / 2 + 1;
+	integer r = v.size;
+	for (;;) { /* H2 */
+		integer k;
+		if (l > 1) {
+			l --;
+			k = index [l];
+		} else { /* l == 1 */
+			k = index [r];
+			index [r] = index [1];
+			r --;
+			if (r == 1) {
+				index [1] = k;
+				break;
+			}
+		}
+		/* H3 */
+		integer i, j = l;
+		for (;;) {
+			/* H4 */
+			i = j;
+			j *= 2;
+			if (j > r)
+				break;
+			if (j < r && compare (v [index [j]], v [index [j + 1]]))
+				j ++; /* H5 */
+			index [i] = index [j]; /* H7 */
+		}
+		for (;;) {  /*H8' */
+			j = i;
+			i = j >> 1;
+			/* H9' */
+			if (j == l || compare (v [k], v [index [i]])) {
+				index [j] = k;
+				break;
+			}
+			index [j] = index [i];
+		}
+	}
+}
+
+inline void INTVECindex_inout (INTVEC index, constINTVEC const& v) {
+	INTVECindex2_inout<constINTVEC, integer> (index, v, [](integer const& x, integer const& y) -> bool { return x < y;});
+}
+
+inline void INTVECindex_inout (INTVEC index, constVEC const& v) {
+	INTVECindex2_inout<constVEC, double> (index, v, [](double const& x, double const& y) -> bool { return x < y;});
+}
+
+inline void INTVECindex_inout (INTVEC index, STRVEC const& v) {
+	INTVECindex2_inout <STRVEC, conststring32> (index, v, [] (conststring32 const& x, conststring32 const& y) -> bool { return Melder_cmp (x, y) < 0;});
+}
+
+inline void INTVECindex_inout (INTVEC index, constSTRVEC const& v) {
+	INTVECindex2_inout <constSTRVEC, conststring32> (index, v, [] (conststring32 const& x, conststring32 const& y) -> bool { return Melder_cmp (x, y) < 0;});
+}
+
+inline integer DigitstringNumber_compare (DigitstringNumber const& x, DigitstringNumber const& y) {
+	return ( x.value < y.value ? -1 : x.value > y.value ? 1 : 
+		(x.numberOfLeadingZeros > y.numberOfLeadingZeros ? - 1 : x.numberOfLeadingZeros < y.numberOfLeadingZeros ? 1 : 0));
+}
+
+inline void INTVECindex_inout (INTVEC index, constDigitstringNumberVEC const& v) {
+	INTVECindex2_inout<constDigitstringNumberVEC, DigitstringNumber> (index, v, [](DigitstringNumber const& x, DigitstringNumber const& y) -> bool
+		{ return ( x.value < y.value ? true : x.value > y.value ? false : 
+		(x.numberOfLeadingZeros > y.numberOfLeadingZeros ? true : false) );}
+	);
+}
 
 void INTVECindex_num_alpha (INTVEC const& target, constSTRVEC const& v, kStrings_sorting sorting);
 
 inline autoINTVEC newINTVECindex (constVEC const& a) {
 	autoINTVEC result = raw_INTVEC (a.size);
-	INTVECindex (result.get(), a);
+	INTVECindex_inout (result.get(), a);
 	return result;
 }
 
 inline autoINTVEC newINTVECindex (constINTVEC const& a) {
 	autoINTVEC result = raw_INTVEC (a.size);
-	INTVECindex (result.get(), a);
+	INTVECindex_inout (result.get(), a);
 	return result;
 }
 
-inline autoINTVEC newINTVECindex (constSTRVEC const& s) {
-	autoINTVEC result = raw_INTVEC (s.size);
-	INTVECindex (result.get(), s);
+inline autoINTVEC newINTVECindex (constSTRVEC const& a) {
+	autoINTVEC result = raw_INTVEC (a.size);
+	INTVECindex_inout (result.get(), a);
 	return result;
 }
 
--- dwsys/NUMsort2.cpp
+++ dwsys/NUMsort2.cpp
@@ -25,16 +25,6 @@
 #include "NUM2.h"
 #include "melder.h"
 
-/*
-	NUMsort2 uses heapsort to sort the second array in parallel with the first one.
-
-	Algorithm follows p. 145 and 642 in:
-	Donald E. Knuth (1998): The art of computer programming. Third edition. Vol. 3: sorting and searching.
-		Boston: Addison-Wesley, printed may 2002.
-	Modification: there is no distinction between record and key and
-	    Floyd's optimization (page 642) is used.
-*/
-
 void MATrankColumns (MAT m, integer cb, integer ce) {
 	Melder_assert (cb > 0 && cb <= m.ncol);
 	Melder_assert (ce > 0 && ce <= m.ncol);
@@ -52,170 +42,6 @@ void MATrankColumns (MAT m, integer cb, integer ce) {
 			m [index [i]] [j] = v [i];
 	}
 }
-
-template <class T>
-void NUMindexx (const T a [], integer n, integer index [], int (*compare) (void *, void *)) {
-	T min;
-	for (integer j = 1; j <= n; j ++)
-		index [j] = j;
-
-	if (n < 2)
-		return;   // Already sorted
-	if (n == 2) {
-		if (COMPARELT (a [2], a [1])) {
-			index [1] = 2;
-			index [2] = 1;
-		}
-		return;
-	}
-	if (n <= 12) {
-		for (integer i = 1; i < n; i ++) {
-			integer imin = i;
-			min = a [index [imin]];
-			for (integer j = i + 1; j <= n; j ++) {
-				if (COMPARELT (a [index [j]], min)) {
-					imin = j;
-					min = a [index [j]];
-				}
-			}
-			const integer ii = index [imin];
-			index [imin] = index [i];
-			index [i] = ii;
-		}
-		return;
-	}
-	// H1
-	integer l = n / 2 + 1, r = n;
-	for (;;) { // H2
-		integer k, i;
-		if (l > 1) {
-			l --;
-			k = index [l];
-		} else { // l == 1
-			k = index [r];
-			index [r] = index [1];
-			r --;
-			if (r == 1) {
-				index [1] = k;
-				break;
-			}
-		}
-		// H3
-		integer j = l;
-		for (;;) {
-			// H4
-			i = j;
-			j *= 2;
-			if (j > r)
-				break;
-
-			if (j < r && COMPARELT (a [index [j]], a [index [j + 1]]))
-				j++; // H5
-
-			index [i] = index [j]; // H7
-		}
-		for (;;) { // H8' 
-			j = i;
-			i = j >> 1;
-			// H9'
-			if (j == l || COMPARELT (a [k], a [index [i]])) {
-				index [j] = k;
-				break;
-			}
-			index [j] = index [i];
-		}
-	}
-}
-
-
-#define MACRO_NUMindex(TYPE) \
-{ \
-	Melder_assert (v.size == index.size); \
-	integer l, r, i, j, ii, k, imin; \
-	TYPE min; \
-	to_INTVEC_out (index); \
-	if (v.size < 2) \
-		return;   /* Already sorted. */ \
-	if (v.size == 2) { \
-		if (COMPARELT (v [2], v [1])) { \
-			index [1] = 2; \
-			index [2] = 1; \
-		} \
-		return; \
-	} \
-	if (v.size <= 12) { \
-		for (i = 1; i < v.size; i ++) { \
-			imin = i; \
-			min = v [index [imin]]; \
-			for (j = i + 1; j <= v.size; j ++) { \
-				if (COMPARELT (v [index [j]], min)) { \
-					imin = j; \
-					min = v [index [j]]; \
-				} \
-			} \
-			ii = index [imin]; \
-			index [imin] = index [i]; \
-			index [i] = ii; \
-		} \
-		return; \
-	} \
-	/* H1 */ \
-	l = v.size / 2 + 1; \
-	r = v.size; \
-	for (;;) { /* H2 */ \
-		if (l > 1) { \
-			l --; \
-			k = index [l]; \
-		} else { /* l == 1 */ \
-			k = index [r]; \
-			index [r] = index [1]; \
-			r --; \
-			if (r == 1) { \
-				index [1] = k; \
-				break; \
-			} \
-		} \
-		/* H3 */ \
-		j = l; \
-		for (;;) { \
-			/* H4 */ \
-			i = j; \
-			j *= 2; \
-			if (j > r) \
-				break; \
-			if (j < r && COMPARELT (v [index [j]], v [index [j + 1]])) \
-				j ++; /* H5 */ \
-			index [i] = index [j]; /* H7 */ \
-		} \
-		for (;;) {  /*H8' */ \
-			j = i; \
-			i = j >> 1; \
-			/* H9' */ \
-			if (j == l || COMPARELT (v [k], v [index [i]])) { \
-				index [j] = k; \
-				break; \
-			} \
-			index [j] = index [i]; \
-		} \
-	} \
-}
-
-#define COMPARELT(x,y) ((x) < (y))
-
-void INTVECindex (INTVEC const& index, constVEC const& v)
-MACRO_NUMindex (double)
-
-void INTVECindex (INTVEC const& index, constINTVEC const& v)
-MACRO_NUMindex (integer)
-
-#undef COMPARELT
-#define COMPARELT(x,y) (Melder_cmp (x,y) <  0)
-
-void INTVECindex (INTVEC const& index, constSTRVEC const& v)
-MACRO_NUMindex (const char32_t *)
-
-#undef COMPARELT
-#undef MACRO_INDEXX
 
 void VECsort3_inplace (VEC const& a, INTVEC const& iv1, INTVEC const& iv2, bool descending) {
 	Melder_assert (a.size == iv1.size && a.size == iv2.size);
@@ -249,6 +75,7 @@ autoINTMAT sortRows_INTMAT (constINTMAT mat, integer icol) { // TODO test me
 		autoINTMAT result = raw_INTMAT (mat.nrow, mat.ncol);
 		for (integer irow = 1; irow <= mat.nrow; irow ++)
 			result [index [irow]]  <<= mat [irow];
+		return result;
 	} catch (MelderError) {
 		Melder_throw (U"Matrix not sorted.");
 	}
