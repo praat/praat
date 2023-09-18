5bf43abf3c44e7a2da8b6979f17fe0f8bbccf457
PaulBoersma <paul.boersma@uva.nl>
2023-09-18 17:58:19 +0200

    editor menu can add separators, headers and indent

--- fon/manual_whatsnew.cpp
+++ fon/manual_whatsnew.cpp
@@ -29,6 +29,10 @@ R"~~~(
 
 Latest changes in Praat.
 
+##6.3.18# (xx September 2023)
+• @SpeechSynthesizer: included the new version of eSpeak (development 1.52), which has 133 languages.
+• @@Add to menu...@: you can now add separators, headers and indented submenus to menus in editor windows.
+
 ##6.3.17# (10 September 2023)
 • SoundEditor: three Paste options, namely ##Paste before selection# (Shift-Command-V),
   ##Paste over selection# (Option-Command-V), and the already existing ##Paste after selection#
