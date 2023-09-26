e0d6cb19f903f639e59c7bc579bef7c2849fa791
davidweenink <david.weenink@uva.nl>
2023-09-26 11:34:52 +0200

    refactoring

---
+++ sensors/praat_sensors_init.h
@@ -0,0 +1,26 @@
+#ifndef _praat_sensors_init_h_
+#define _praat_sensors_init_h_
+/* praat_sensors_init.h
+ *
+ * Copyright (C) 2023 David Weenink
+ *
+ * This code is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This code is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this work. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "praatM.h"
+
+void praat_sensors_init ();
+
+#endif
+/* End of file praat_sensors_init.h */
