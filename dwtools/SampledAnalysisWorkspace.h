1515f8e258091f7bf2c6a3e04dd3b0c09a5346ed
davidweenink <david.weenink@uva.nl>
2024-05-25 21:09:19 +0200

    further abstraction SampledAnalysisWorkspace

---
+++ dwtools/SampledAnalysisWorkspace.h
@@ -0,0 +1,52 @@
+#ifndef _SampledAnalysisWorkspace_h_
+#define _SampledAnalysisWorkspace_h_
+/* SampledAnalysisWorkspace.h
+ *
+ * Copyright (C) 2024 David Weenink
+ *
+ * This code is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This code is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this work. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "Sampled.h"
+#include "Sound.h"
+
+#include "SampledAnalysisWorkspace_def.h"
+
+void ExtraAnalysisData_init (ExtraAnalysisData me, SampledAnalysisWorkspace thee);
+
+autoWorkvectorPool WorkvectorPool_create (INTVEC const& vectorSizes, bool reusable);
+
+void SampledAnalysisWorkspace_init (SampledAnalysisWorkspace me, Sampled input, Sampled output);
+
+autoSampledAnalysisWorkspace SampledAnalysisWorkspace_create (Sampled input, Sampled output);
+
+//void SampledAnalysisWorkspace_initExtraAnalysisData (SampledAnalysisWorkspace me, ExtraAnalysisData thee);
+
+void SampledAnalysisWorkspace_initWorkvectorPool (SampledAnalysisWorkspace me, INTVEC const& vectorSizes);
+
+void SampledAnalysisWorkspace_getThreadingInfo (SampledAnalysisWorkspace me, integer maximumNumberOfThreads, integer numberOfFramesPerThread, integer *out_numberOfThreads);
+
+void SampledAnalysisWorkspace_replaceInput (SampledAnalysisWorkspace me, Sampled input);
+/*
+	Preconditions:
+		my input->xmin/xmax = thy xmin/xmax
+		my input->nx = thy nx
+		my input->dx = thy dx
+		my input->x1 = thy x1
+*/
+
+void SampledAnalysisWorkspace_analyseThreaded (SampledAnalysisWorkspace me);
+
+#endif /* _SampledAnalysisWorkspace_h_ */
+ 
---
+++ dwtools/SampledAnalysisWorkspace_def.h
@@ -0,0 +1,139 @@
+/* SampledAnalysisWorkspace_def.h
+ *
+ * Copyright (C) 2024 David Weenink
+ *
+ * This code is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This code is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this work. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+	These forwad declarations are needed because we define new classes with reference to each other
+*/
+typedef struct structSampledAnalysisSpecificData *SampledAnalysisSpecificData;
+typedef const struct structSampledAnalysisSpecificData *constSampledAnalysisSpecificData;
+typedef autoSomeThing<structSampledAnalysisSpecificData> autoSampledAnalysisSpecificData;
+
+#define ooSTRUCT WorkvectorPool
+oo_DEFINE_CLASS (WorkvectorPool, Daata)
+
+	oo_INTEGER (poolMemorySize)
+	oo_INTEGER (numberOfVectors)
+	oo_INTVEC (vectorSizes, numberOfVectors)
+	oo_INTVEC (vectorStart, numberOfVectors)
+	oo_VEC (memoryPool, poolMemorySize)
+	oo_BOOLEAN (reusable)
+	oo_BOOLVEC (inuse, numberOfVectors)
+	
+	#if oo_DECLARING
+	
+		VEC getRawVEC (integer vectorIndex, integer size) {
+			Melder_assert (vectorIndex > 0 && vectorIndex <= numberOfVectors);
+			Melder_assert (size <= vectorSizes [vectorIndex]);
+			Melder_assert (! reusable || (reusable && ! inuse [vectorIndex]));
+			inuse [vectorIndex] = true;
+			return memoryPool.part (vectorStart [vectorIndex], size);
+		}
+		
+		MAT getRawMAT (integer vectorIndex, integer nrow, integer ncol) {
+			Melder_assert (vectorIndex > 0 && vectorIndex <= numberOfVectors);
+			const integer wantedSize = nrow * ncol;
+			Melder_assert (wantedSize <= vectorSizes [vectorIndex]);
+			inuse [vectorIndex] = true;
+			VEC pool = memoryPool.part (vectorStart [vectorIndex], wantedSize);
+			return pool.asmatrix (nrow, ncol);
+		}
+		
+		void freeVEC (integer vectorIndex) {
+			Melder_assert (vectorIndex > 0 && vectorIndex <= numberOfVectors);
+			Melder_assert (inuse [vectorIndex]);
+			inuse [vectorIndex] = false;
+		}
+		
+	#endif
+
+oo_END_CLASS (WorkvectorPool)
+#undef ooSTRUCT
+
+
+/*
+	A separate deep copy of the SampledAnalysisWorkspace is needed for each thread
+*/
+#define ooSTRUCT SampledAnalysisWorkspace
+oo_DEFINE_CLASS (SampledAnalysisWorkspace, Daata)
+
+	/*
+		Only a reference to the Sampled that is analysed is needed in a thread because
+		each thread only accesses disjoint parts of it.
+	*/
+	oo_UNSAFE_BORROWED_TRANSIENT_CONST_OBJECT_REFERENCE (Sampled, input)
+
+	/*
+		Each thread accesses the same disjoint parts in the result.
+		Precondition:
+			input -> xmin == output -> xmin;
+			input -> xmax == output -> xmax;
+	*/
+	oo_UNSAFE_BORROWED_TRANSIENT_MUTABLE_OBJECT_REFERENCE (Sampled, output)
+	
+	oo_BOOLEAN (useMultiThreading)
+	oo_INTEGER (minimumNumberOfFramesPerThread) // 40 ?
+	oo_INTEGER (maximumNumberOfThreads)
+	
+	oo_BOOLEAN (frameAnalysisIsOK)
+	oo_INTEGER (globalFrameErrorCount)			// the number of frames where some error occured
+	oo_INTEGER (currentFrame)					// the frame we are working on
+	
+	/*
+		For approximations we need tolerances
+	*/
+	oo_DOUBLE (tol1)
+	oo_DOUBLE (tol2)
+		
+	/*
+		For all temporary work vectors in the analysis
+	*/
+	oo_OBJECT (WorkvectorPool, 0, workvectorPool)
+	
+	oo_OBJECT (SampledAnalysisSpecificData, 0, specificData)
+	
+	#if oo_DECLARING
+		void (*getInputFrame) (SampledAnalysisWorkspace me, integer iframe);
+		void (*analyseOneInputFrame) (SampledAnalysisWorkspace me); // has to set frameAnalysisIsOK to true or false
+		void (*allocateOutputFrames) (SampledAnalysisWorkspace me);
+	#endif
+		
+	#if oo_COPYING
+		thy getInputFrame = getInputFrame;
+		thy analyseOneInputFrame = our analyseOneInputFrame;
+		thy allocateOutputFrames = our allocateOutputFrames;
+	#endif	
+		
+	#if oo_DECLARING
+		
+		void analyseManyInputFrames (SampledAnalysisWorkspace me, integer fromFrame, integer toFrame);
+		
+	#endif
+
+oo_END_CLASS (SampledAnalysisWorkspace)
+#undef ooSTRUCT
+
+
+#define ooSTRUCT SampledAnalysisSpecificData
+oo_DEFINE_CLASS (SampledAnalysisSpecificData, Daata)
+	oo_UNSAFE_BORROWED_TRANSIENT_CONST_OBJECT_REFERENCE (SampledAnalysisWorkspace, analysisWorkspace)
+	oo_OBJECT (WorkvectorPool, 0, workvectorPool)
+oo_END_CLASS (SampledAnalysisSpecificData)
+#undef ooSTRUCT
+
+/* End of file SampledAnalysisWorkspace_def.h */
+ 
