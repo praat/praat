3ef22a0d62bce7be2b3fa189b53e5ba4f0d9b208
PaulBoersma <paul.boersma@uva.nl>
2025-01-03 17:13:21 +0100

    Notebook: table column separator requires spaces (because "|" coudl be link separator)

--- sys/ManPages.cpp
+++ sys/ManPages.cpp
@@ -606,7 +606,7 @@ static void readOnePage_notebook (ManPages me, MelderReadText text) {
 				MelderString_append (& buffer_graphical, U"\t");
 				line += 2;
 				while (*line != U'\0') {
-					if (*line == U'|')
+					if (line [0] == U'|' && Melder_isHorizontalSpace (line [-1]) && (Melder_isHorizontalSpace (line [1]) || line [1] == U'\0'))
 						MelderString_appendCharacter (& buffer_graphical, U'\t');
 					else
 						MelderString_appendCharacter (& buffer_graphical, *line);
