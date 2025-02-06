5dd844f19df4af77f34e0c8965a6689379d2c526
davidweenink <david.weenink@uva.nl>
2025-02-07 00:01:48 +0100

    faster median calculation O(n)

---
+++ dwsys/NUMmedian.cpp
@@ -0,0 +1,114 @@
+/* NUMmedian.cpp
+ *
+ * Copyright (C) 2025 David Weenink
+ *
+ * This code is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This code is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this work. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "NUMmedian.h"
+#include "median_of_ninthers.h"
+#include "Permutation.h"
+
+void timeMedian (void) {
+    try {
+        Melder_clearInfo ();
+        autoINTVEC numberOfElements {11_integer, 101_integer, 1001_integer, 10001_integer, 100001_integer};
+        MelderInfo_writeLine (U"n tNew tOld tOld/tNew info");
+        for (integer index = 1; index <= numberOfElements.size; index ++) {
+            /*
+                median of increasing values
+             */
+            const integer n = numberOfElements [index];
+            autoVEC p = to_VEC (n);
+            autoVEC pc = copy_VEC (p.get());
+            Melder_stopwatch ();
+            sort_e_VEC_inout (pc.get());
+            double  median1 = NUMquantile (pc.get(), 0.5);
+            double t1 = Melder_stopwatch ();
+            double median2 = num::NUMquantile (p.get(), 0.5);
+            double t2 = Melder_stopwatch ();
+            Melder_assert (median1 == median2);
+            MelderInfo_writeLine (n, U" ", t2, U" ", t1, U" *", t1 / t2, U"* increasing");
+            /*
+                median of decreasing values
+            */
+            for (integer i = 1; i <= n; i ++) {
+                p [i] = n + 1 - i;
+            }
+            pc = copy_VEC (p.get());
+            Melder_stopwatch ();
+            sort_e_VEC_inout (pc.get());
+            median1 = NUMquantile (pc.get(), 0.5);
+            t1 = Melder_stopwatch ();
+            median2 = num::NUMquantile (p.get(), 0.5);
+            t2 = Melder_stopwatch ();
+            Melder_assert (median1 == median2);
+            MelderInfo_writeLine (n, U" ", t2, U" ", t1, U" *", t1 / t2, U"* decreasing");
+            /*
+                median of increasing values followed by decreasing values
+            */
+            for (integer i = 1; i <= n/2; i ++) {
+                p [i] = i;
+                p [n/2 + i] = n/2 + 1 - i;
+            }
+            pc = copy_VEC (p.get());
+            Melder_stopwatch ();
+            sort_e_VEC_inout (pc.get());
+            median1 = NUMquantile (pc.get(), 0.5);
+            t1 = Melder_stopwatch ();
+            median2 = num::NUMquantile (p.get(), 0.5);
+            t2 = Melder_stopwatch ();
+            Melder_assert (median1 == median2);
+            MelderInfo_writeLine (n, U" ", t2, U" ", t1, U" *", t1 / t2, U"* inc-dec");
+            /*
+                median of uniform random values
+             */
+            for (integer i = 1; i <= n; i ++) {
+                p [i] = NUMrandomUniform (-100.0, 100.0);
+            }
+            pc = copy_VEC (p.get());
+            Melder_stopwatch ();
+            sort_e_VEC_inout (pc.get());
+            median1 = NUMquantile (pc.get(), 0.5);
+            t1 = Melder_stopwatch ();
+            median2 = num::NUMquantile (p.get(), 0.5);
+            t2 = Melder_stopwatch ();
+            Melder_assert (median1 == median2);
+            MelderInfo_writeLine (n, U" ", t2, U" ", t1, U" *", t1 / t2, U"* uniform(-100,100)");
+            /*
+                median of Gaussian random values
+             */
+            for (integer i = 1; i <= n; i ++) {
+                p [i] = NUMrandomGauss (0.0, 1.0);
+            }
+            pc = copy_VEC (p.get());
+            Melder_stopwatch ();
+            sort_e_VEC_inout (pc.get());
+            median1 = NUMquantile (pc.get(), 0.5);
+            t1 = Melder_stopwatch ();
+            median2 = num::NUMquantile (p.get(), 0.5);
+            t2 = Melder_stopwatch ();
+            Melder_assert (median1 == median2);
+            MelderInfo_writeLine (n, U" ", t2, U" ", t1, U" *", t1 / t2, U"* gauss(0,1)");
+        }
+
+        MelderInfo_close ();
+
+    } catch (MelderError) {
+        Melder_throw (U"Could not perform timing of medians.");
+    }
+}
+
+
+ /*  End of file NUMmedian.cpp */
---
+++ dwsys/NUMmedian.h
@@ -0,0 +1,66 @@
+#ifndef _NUMmedian_h_
+#define _NUMmedian_h_
+/* NUMmedian.h
+ *
+ * Copyright (C) 2024-2025 David Weenink
+ *
+ * This code is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This code is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this work. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "median_of_ninthers.h"
+#include "melder.h"
+#include "NUM2.h"
+
+/*
+	Select the k-th largest element from the vector.
+	The algorithm is from:
+	Alexandrescu, Andrei. Fast deterministic selection, Symposium on experimental algorithms (SEA 2017), pp. 1-18.
+*/
+
+namespace num {
+	template <typename T>
+	inline void NUMselect_inplace (vector<T> const& v, integer kth_asIfSorted) {
+		adaptiveQuickselect (v.asArgumentToFunctionThatExpectsZeroBasedArray (), kth_asIfSorted - 1, v.size);
+	}
+
+	template <typename T>
+	T NUMselect (constvector<T> const& v, integer kth_asIfSorted) {
+		autovector<T> vc = copy_VEC (v);
+		adaptiveQuickselect (vc.asArgumentToFunctionThatExpectsZeroBasedArray (), kth_asIfSorted - 1, v.size);
+		return vc [kth_asIfSorted];
+	}
+
+	template <typename T>
+	double NUMquantile (vector<T> const& v, double factor) {
+		if (v.size < 1)
+			return undefined;
+		if (v.size == 1)
+			return v [1];
+		const double place = factor * v.size + 0.5;
+		const integer left = Melder_clipped (1_integer, Melder_ifloor (place), v.size - 1);
+		NUMselect_inplace (v, left);
+		const integer minPos = NUMminPos (v.part (left + 1, v.size));
+		const double slope = v [left + minPos] - v [left];
+		if (slope == 0.0)
+			return v [left];   // or a [left + 1], which is the same
+		return v [left] + (place - left) * slope;
+	}
+}
+
+/* Only for timing the algorithms */
+
+void timeMedian (void);
+
+
+#endif /* _NUMmedian_h_ */
