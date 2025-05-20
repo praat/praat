ca9d5922f5ef76e2df3bfdc1c1d656c5b3918ac0
PaulBoersma <paul.boersma@uva.nl>
2025-05-20 21:13:38 +0200

    for version 6.4.34

--- sys/DemoEditor.cpp
+++ sys/DemoEditor.cpp
@@ -189,7 +189,7 @@ int Demo_show () {
 	autoDemoOpen demo;
 	GuiThing_show (theReferenceToTheOnlyDemoEditor -> windowForm);
 	Graphics_updateWs (theReferenceToTheOnlyDemoEditor -> graphics.get());
-	#if defined (macintosh)
+	#if defined (macintosh) || defined (UNIX)
 		Melder_sleep (0.02);   // because GuiShell_drain is not guaranteed to drain if called within 16 ms from previous
 	#endif
 	GuiShell_drain (theReferenceToTheOnlyDemoEditor -> windowForm);
