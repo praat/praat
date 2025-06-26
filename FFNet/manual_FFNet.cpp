fa9414afcfa08ef4b2206e4e14b8ffbf3eba0d38
PaulBoersma <paul.boersma@uva.nl>
2025-06-26 10:23:24 +0200

    manual GPL 3, because of docs folder

--- FFNet/manual_FFNet.cpp
+++ FFNet/manual_FFNet.cpp
@@ -1,10 +1,10 @@
 /* manual_FFNet.c
  *
- * Copyright (C) 1994-2019 David Weenink
+ * Copyright (C) 1994-2019.2023,2025 David Weenink
  *
  * This code is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or (at
+ * the Free Software Foundation; either version 3 of the License, or (at
  * your option) any later version.
  *
  * This code is distributed in the hope that it will be useful, but
